f93d90164f80a2e4545f8814b0e949acaf1a7a7a
Michaela Trčková <misa.trckova@volny.cz>
2026-03-29 18:12:29 +0200

    set up basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
-import './App.css';
+import './globals.css';
+import { BackgroundMesh } from './components/Background/Background';
+import { Seal } from './components/Seal';
 
 export default function App() {
-  return <div>Seal Bounce App 🦭</div>;
+  return (
+    <>
+      <BackgroundMesh />
+      <main className="content-container">
+        Seal Bounce App 🦭
+        <Seal />
+      </main>
+    </>
+  );
 }
---
+++ src/components/Background/Background.tsx
@@ -0,0 +1,12 @@
+import './index.css';
+
+export const BackgroundMesh = () => {
+  return (
+    <div className="color-mesh-container">
+      <div className="mesh-point mesh-point-one" />
+      <div className="mesh-point mesh-point-two" />
+      <div className="mesh-point mesh-point-three" />
+      <div className="mesh-point mesh-point-four" />
+    </div>
+  );
+};
